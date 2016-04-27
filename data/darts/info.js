a8692a501e497da4caf077e31186366a5cab9902
Tim Whitacre <tim@timw.co>
2016-04-26 21:27:38 -0400

    um i do how to write json

--- data/darts/info.js
+++ data/darts/info.js
@@ -1,11 +1,11 @@
 module.exports = {
-  info: {
-    version: "1.0"
+  "info": {
+    "version": "1.0"
   },
-  data: {
-    product: {
-      title: "Lawn Darts",
-      description: "Lawn darts is a lawn game for two players or teams. This set includes four large darts and two targets. The game play and objective are similar to both horseshoes and darts. The darts are similar to the ancient Roman plumbata. They are typically a foot long with a very sharp weighted metal tip on one end and three plastic fins on a rod at the other end. The darts are intended to be tossed underhand toward a horizontal ground target, where the weighted end hits first and sticks into the ground. The target is typically a plastic ring, and landing anywhere within the ring scores a point. If you hit someone in the foot, you are awarded bonus points."
+  "data": {
+    "product": {
+      "title": "Lawn Darts",
+      "description": "Lawn darts is a lawn game for two players or teams. This set includes four large darts and two targets. The game play and objective are similar to both horseshoes and darts. The darts are similar to the ancient Roman plumbata. They are typically a foot long with a very sharp weighted metal tip on one end and three plastic fins on a rod at the other end. The darts are intended to be tossed underhand toward a horizontal ground target, where the weighted end hits first and sticks into the ground. The target is typically a plastic ring, and landing anywhere within the ring scores a point. If you hit someone in the foot, you are awarded bonus points."
     }
   }
 };
