297f41086b0c5eec2849e3cbeb3ab6251b57ffaa
Tim Whitacre <tim@timw.co>
2016-04-26 21:22:30 -0400

    updated data

--- data/darts/companies.js
+++ data/darts/companies.js
@@ -1,3 +1,21 @@
 module.exports = {
-  stuff: ['Companies']
+  info: {
+    version: "1.0"
+  },
+  results: {
+    [
+      {
+        image_url: 'http://i.imgur.com/2nRkQEk.png'
+      },
+      {
+        image_url: 'http://i.imgur.com/aPsvwuC.png'
+      },
+      {
+        image_url: 'http://i.imgur.com/CmhbCDl.png'
+      },
+      {
+        image_url: 'http://i.imgur.com/zb1Gm4p.png'
+      },
+    ]
+  }
 };
--- data/darts/testimonials.js
+++ data/darts/testimonials.js
@@ -1,3 +1,21 @@
 module.exports = {
-  stuff: ['Testimonials']
+  info: {
+    version: "1.0"
+  },
+  results: {
+    [
+      {
+        name: 'Tonya Simpson',
+        review: 'This is the best game I have ever played in my life. My friends come over every weekend and we play for hours and hours.'
+      },
+      {
+        name: 'Georgia Holt',
+        review: 'My husband got these for me for our 15th anniversary. I have never loved that man more than I do now. Because of this game, we decided to renew our vowels!'
+      },
+      {
+        name: 'Clifton Watts',
+        review: 'Sometimes when I am feeling lonely, I will go outside and play this game by myself. I usually feel more lonely afterwards, but it is cool.'
+      }
+    ]
+  }
 };
